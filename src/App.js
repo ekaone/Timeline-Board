937ceb4f6592d6e96fb020cf075b1685b67b5527
Eka Prasetia <ekaone3033@gmail.com>
2020-11-24 08:38:10 +0700

    add elements

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  VerticalTimeline,
+  VerticalTimelineElement
+} from "react-vertical-timeline-component";
+import { FiCodesandbox, FiHome } from "react-icons/fi";
+import { timelines } from "./timelines";
+import "react-vertical-timeline-component/style.min.css";
+import "./App.css";
+
+const TimeLineElement = ({ title, location, description, date, id }) => (
+  <VerticalTimelineElement
+    key={id}
+    className="vertical-timeline-element--work"
+    contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+    contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
+    date={date}
+    iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+    icon={<FiCodesandbox />}
+  >
+    <h3 className="vertical-timeline-element-title">{title}</h3>
+    <h4 className="vertical-timeline-element-subtitle">{location}</h4>
+    <p>{description}</p>
+  </VerticalTimelineElement>
+);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <VerticalTimeline>
+        {timelines.map(el => (
+          <TimeLineElement
+            title={el.title}
+            location={el.location}
+            description={el.description}
+            date={el.date}
+            id={el.id}
+          />
+        ))}
+      </VerticalTimeline>
     </div>
   );
 }
